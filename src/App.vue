f476547b3c59327e84344edd4257743574d57ad6
FrancescoPepi <francescopepi16@gmail.com>
2023-09-19 17:18:23 +0200

    Add All File

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  data() {
+    return {};
+  },
+
+  // 	methods:{
+  // 		myMethods(){
+  // 			...
+  // 		},
+  // 	},
+
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+
+  // 	props:{
+  // 		passaggioInfo: stringa,
+  //  	 },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
+
+<style lang="scss"></style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+// ADD SCSS
+import "./style/general.scss";
+
+const app = createApp(App);
+app.mount("#app");
